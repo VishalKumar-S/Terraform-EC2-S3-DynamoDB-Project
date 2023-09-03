96bc246ba47feaadf959fe5caf2c14b7b5b6c69e
Vishal Kumar. S <118868521+VishalKumar-S@users.noreply.github.com>
2023-09-03 13:35:17 +0530

    Create main.tf

---
+++ aws/remote-state/main.tf
@@ -0,0 +1,46 @@
+#Remote backend server
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {}
+
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+#Create s3 bucket
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+#Create Dynamo db table
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
