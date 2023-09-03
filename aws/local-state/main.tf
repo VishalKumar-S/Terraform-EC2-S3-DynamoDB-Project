d93fd919c8a3c3df3fe41010a0207997297ed61f
Vishal Kumar. S <118868521+VishalKumar-S@users.noreply.github.com>
2023-09-03 14:11:22 +0530

    Update main.tf

--- aws/local-state/main.tf
+++ aws/local-state/main.tf
@@ -7,6 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket         = "165617766510-terraform-states"
+    key            = "terraform-project/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    # You can add the kms_key_id and dynamodb_table settings here if needed.
+  }
 }
 
 provider "aws" {
